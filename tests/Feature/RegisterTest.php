0228c886c8a9e10408af7e251d01a83ae334ac7d
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-04-22 12:28:11 +0000

    use pest post namespace

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -1,16 +1,17 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Laravel\post;
 
 uses(RefreshDatabase::class);
 it('has errors if details are not provided', function () {
-    $this->post('/register')
+    post('/register')
         ->assertSessionHasErrors(['name', 'email', 'password']);
 });
 
 
 test('register', function () {
-    $this->post('/register', [
+    post('/register', [
         'name' => 'Kofi Ramos',
         'email' => 'kofiramos@gmail.com',
         'password' => 'hellofresh'
